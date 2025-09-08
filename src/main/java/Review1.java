42acd1053405598707583add4964124e0faa9c00
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-09-08 18:16:06 +0000

    make changes

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,11 +2,19 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+    } 
+	public class Main {
+		public static void main (String{} args) {
+	System.out.println("There once was a man from St. Ives.");
+		}
 	}
+
+
 
 	public static void q2() {
 		//Write question 2 code here
 	}
+	System.out.println
 
 	public static void q3() {
 		//Write question 3 code here
@@ -18,6 +26,11 @@ public class Review1 {
 
 	public static void q5() {
 		//Write question 5 code here
+	}
+
+	@Override
+	public String toString() {
+		return "Review1 []";
 	}
 
 }
