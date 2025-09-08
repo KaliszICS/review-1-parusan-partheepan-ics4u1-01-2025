8da4bbc0603af515fa0d2f36086a4a8df8e99f7a
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-09-08 18:39:31 +0000

    make changes

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,35 +2,42 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
-    } 
-	public class Main {
-		public static void main (String{} args) {
-	System.out.println("There once was a man from St. Ives.");
-		}
-	}
-
-
-
+	  System.out.println("There once was a man from St. Ives.");
+	 }
 	public static void q2() {
 		//Write question 2 code here
-	}
-	System.out.println
+	  int nine = 9;
+	  int five = 5;
+	System.out.println(nine/five);
+	System.out.println(nine % five); 
+   }
+
 
 	public static void q3() {
 		//Write question 3 code here
+	System.out.println("Hello/nhello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	boolean a = true;
+	System.out.println(a);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
-	}
+	String hello = "Hello"; 
+	String world = "World";
+	System.out.println(hello + "");
+	System.out.println(world);
+	};
 
-	@Override
-	public String toString() {
-		return "Review1 []";
-	}
-
+	
+    public static void main(String[] args) {
+        q1();
+        q2();
+        q3();
+        q4();
+        q5();
+}
 }
